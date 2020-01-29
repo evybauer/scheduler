67599a91eb7f464dfb9cdc20b865dfa42581b35f
Evelyn Louise Bauer <evybauer@gmail.com>
2020-01-28 20:38:49 -0800

    Add put and delete mock implementations to return the status code and response message

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -83,17 +83,15 @@ export default {
     }
   }),
   put: jest.fn(url => {
-    if (url === "/api/appointments/1") {
       return Promise.resolve({
         status: 204,
         statusText: "No Content"
       });
-    }
-    if (url === "/api/appointments") {
-      return Promise.resolve({
-        status: 204,
-        statusText: "No Content"
-      });
-    }
+    }),
+  delete: jest.fn(url => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content"
+    });
   })
 };
